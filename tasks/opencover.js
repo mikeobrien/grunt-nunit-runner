0488f69a041121f9a79eb97ad0f305aede51677f
Christian Meilke <cme@auctionata.com>
2015-03-16 10:56:24 +0100

    More Opencover options are configurable from outside now

--- tasks/opencover.js
+++ tasks/opencover.js
@@ -4,7 +4,21 @@ exports.wrapCommand = function(command, options) {
     wrappedArgs.push('-target:"' + command.path + '"');
     wrappedArgs.push('-targetargs:"' + command.args.join(' ') + '"');
     wrappedArgs.push('-register:user');
-    wrappedArgs.push('-output:_CodeCoverageResult.xml');
+    wrappedArgs.push('-returntargetcode');
+    
+    var coverReportFilePath = '_CodeCoverageResult.xml';
+    if (options.coverReportFilePath && options.coverReportFilePath !== '') {
+        coverReportFilePath = '"' + options.coverReportFilePath + '"';
+    }
+    wrappedArgs.push('-output:' + coverReportFilePath);
+
+    if (options.coverFilter && options.coverFilter !== '') {
+        wrappedArgs.push('-filter:"' + options.coverFilter + '"');
+    }
+
+    if (options.coverExcludeAttributeFilter && options.coverExcludeAttributeFilter !== '') {
+        wrappedArgs.push('-excludebyattribute:"' + options.coverExcludeAttributeFilter + '"');
+    }
 
     return {
         path: pathToOpencover,
